76c59408c0f1d4d4b38bb353a06c23df0281296b
jenrrysd <dextre1481@gmail.com>
2025-05-17 01:14:15 -0500

    nuevos cambios

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function() {
             showError('Error de conexión. Por favor, intenta nuevamente.');
         };
         
-        xhr.open('POST', '/comprimir', true);
+        xhr.open('POST', 'https://comprimirpdf.dextre.xyz/comprimir', true);
         xhr.send(formData);
     }
     
